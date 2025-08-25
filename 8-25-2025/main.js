07cc6f1b1b6b444755f01eb2c4b7c890e7fc94f7
laptop <mr.noone102@gmail.com>
2025-08-24 21:46:28 -0700

    vereables

---
+++ 8-25-2025/main.js
@@ -0,0 +1,19 @@
+// thsi var it is not used now
+
+// var name = 'fakhir';
+// console.log(name);
+
+// var name = 'amir';
+// console.log(name);
+
+// now a days we use const and let
+
+// (let) you can change it's value 
+
+let name = 'fakhir';
+console.log(name);
+
+// (const) you can't change it's value 
+
+const Name = 'amir';
+console.log(Name);

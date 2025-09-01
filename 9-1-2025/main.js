3d0a5fd83c27765a2f1361e2a7c49e79f5254dee
laptop <mr.noone102@gmail.com>
2025-09-01 04:33:45 -0700

    class 9-1-2025

---
+++ 9-1-2025/main.js
@@ -0,0 +1,57 @@
+// arry
+
+let student = ["fakhir", "humza","amna","wasi",123];
+
+// how add 
+
+console.log(student.push("ali"));
+
+// how delete last one
+
+// console.log(student.pop("ali"));
+
+// how to delete data from first and add
+
+
+// let newuser  = student.shift();
+// let newuser  = student.unshift();
+
+// how to choose range to display
+
+// let newuser = student.slice(5,6)
+
+// how to change data of object
+
+// let newuser = student.splice(0,3);  //it will delete fakhir to amna
+
+// how add data 
+
+// let newuser = student.splice(1,0,"taha")
+
+// // how to check data
+
+// let check = student.includes("fakhr"); // it well display data if you write the data right
+// console.log(check);
+
+// // how to reverse data
+
+// console.log(student.reverse());
+
+// // how to sort data in a,b,c
+
+// console.log(student.sort());
+
+// console.log(newuser);
+// console.log(student);
+
+// object
+
+// let object = {
+//   name:"fakhir",
+//   age:22,
+//   class:12
+// }
+
+// console.log(object.name);
+
+
